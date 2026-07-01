c649cba081f4c328f7b5ea53c28ba00cb0bbb7a1
syedk25 <k.syed0787@gmail.com>
2026-07-01 09:31:16 +0530

    landing page

--- 3)TAILWIND CSS/landing-page/src/coponents/Main.jsx
+++ 3)TAILWIND CSS/landing-page/src/coponents/Main.jsx
@@ -5,8 +5,12 @@ import backend from '../assets/Backend.svg'
 
 function Main() {
   return (
-    <div className='container mx-auto flex text-center mt-12 p-2 flex-col md:flex-row sm:gap-12'>
-        
+    <div className='container mx-auto flex text-center mt-12 p-2 flex-col md:flex-row sm:gap'>
+        <div className='md:w-1/3 flex flex-col items-center'>
+            <h1 className='text-2xl'>Web Developer</h1>
+            <img className='h-40 w-40' src={Web} alt="" />
+            <p className='font-sans'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde sint illo omnis veniam numquam soluta impedit ipsum laboriosam voluptatem nemo!</p>
+        </div>
         <div className='md:w-1/3 flex flex-col items-center'>
             <h1 className='text-2xl'>Frontend Developer</h1>
             <img className='h-40 w-40' src={frontend} alt="" />
