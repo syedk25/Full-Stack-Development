8056f41770ec9a172984625ffe87c4506353bf11
syedk25 <k.syed0787@gmail.com>
2026-08-20 21:36:13 +0530

    save

---
+++ 9)Java/2)Intermediate/TreeSetEx.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class TreeSetEx {
+ public static void main(String[] args) {
+  
+  
+  Set<Integer> nums=new TreeSet<Integer>();
+
+  nums.add(3);
+  nums.add(2);
+  nums.add(1);
+  System.out.println(nums);
+ }
+ 
+}
---
+++ 9)Java/ArrrayListEx.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class ArrrayListEx {
+
+    public static void main(String[] args)
+    {
+        // Creating a List of Strings using ArrayList
+        List<String> li = new ArrayList<>();
+
+        // Adding elements in List
+        li.add("Java");
+        li.add("Python");
+        li.add("DSA");
+        li.add("C++");
+ 
+System.out.println(li);
+
+}
+}
