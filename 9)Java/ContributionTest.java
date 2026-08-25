72b00b8b8afcf9f601404bd39ce1741ba888fc8c
syedk25 <k.syed0787@gmail.com>
2026-08-25 14:31:01 +0530

    Add contribution test file

---
+++ 9)Java/ContributionTest.java
@@ -0,0 +1,7 @@
+public class ContributionTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub Contribution Graph! 🚀");
+        System.out.println("Full Stack Developer: syedk25");
+        System.out.println("Stack: React + Spring Boot + MySQL");
+    }
+}
